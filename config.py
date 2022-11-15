c08ab7c80ece578880ef0fe74870544305e8b02e
henrythomasth12 <107455101+henrythomasth12@users.noreply.github.com>
2022-11-14 19:33:37 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+#[twitter]
+
+api_key = "Vd0NeK5yAyu84sz8ZruOoNzhT"
+api_key_secret = "hxbIR4sbwifEyuOMatKA3YJMOBr3Pb7QEQBdnyc7BORVeMrDZZ"
+
+access_token = "2863956500-K3zSh2oH8P5Oi2Z79xf4S4lhSWLgfpqpCChONIu"
+access_token_secret = "1qZzsXsg4Ha7bKZIjZcWnJ13JNrQ85Wd1eYBVRYtaQr8B"
+
